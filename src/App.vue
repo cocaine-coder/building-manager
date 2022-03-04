feec88f48219194a6afd02a39363717cf9111a4d
tracy <tracywang1997@outlook.com>
2022-03-04 20:59:24 +0800

    添加board自定义控件

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,55 @@
 import { Scene } from '@babylonjs/core';
 import SceneComponent from './components/SceneComponent.vue';
 import SceneManager from './manager/SceneManager';
+import Board from './components/base/Board.vue';
 
-function onSceneReady(scene:Scene){
+function onSceneReady(scene: Scene) {
   SceneManager.Instance.init(scene);
+}
+
+function handleTest(){
+  console.log(123);
 }
 
 </script>
 
 <template>
-   <SceneComponent
-        className="scene-container"
-        :onSceneReady="onSceneReady"
-        :onRender="undefined"
-        antialias
-        adaptToDeviceRatio></SceneComponent>
+  <SceneComponent
+    class="scene-container"
+    :onSceneReady="onSceneReady"
+    :onRender="undefined"
+    antialias
+    adaptToDeviceRatio
+  ></SceneComponent>
+
+  <div id="controls">
+    <Board width="30px" height="300px" :alpha="0.1" radius="15px">
+      <button @click="handleTest">click</button>
+    </Board>
+  </div>
 </template>
 
 <style>
-html,body{
-  margin:0;
+html,
+body {
+  margin: 0;
+  height: 100vh;
 }
 
 #app {
   height: 100vh;
 }
 
-.scene-container{
-  height : 100vh;
+.scene-container {
+  height: 100vh;
   width: 100%;
+}
+
+#controls {
+  position:absolute;
+  top: 0;
+  width:100%;
+  height: 100%;
+  pointer-events:none;
 }
 </style>
---
+++ src/components/base/Board.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+    width:{
+        default:''
+    },
+    height:{
+        default:''
+    },
+    alpha:{
+        default: 0.5
+    },
+    radius:{
+        default: '1px'
+    }
+})
+
+const aaa =1;
+</script>
+
+<template>
+    <div :style="{ width: width, height: height }" class="board">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.board {
+    background: rgba(29, 29, 31, v-bind(alpha));
+    backdrop-filter: saturate(200%) blur(10px) brightness(50%);
+    pointer-events:all;
+    border-radius:v-bind(radius)
+}
+</style>
